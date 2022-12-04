5183d765b2674b7b93eb42f0b3d99cf2d3acdc5a
richsi <rzhsieh2013@gmail.com>
2022-12-03 23:49:51 -0800

    added join and leave commands

--- sbotify/sbotify.py
+++ sbotify/sbotify.py
@@ -1,15 +1,43 @@
 import os
-import nextcord
+import discord
 from discord.ext import commands
+from discord import Interaction
 
-TOKEN = "MTA0ODc3MTA2NjcxNTE4OTM0OA.Gn9yKd.PsCa3mZ-DIhybD-YzgLosSaCRLDVQ3DDAxmD-8"
+intents = discord.Intents.all()
+intents.message_content = True
+
+TOKEN = "MTA0ODc3MTA2NjcxNTE4OTM0OA.GlRYJ-.CXng3AA5FJdwEPx1sHPtGtlTiA0K7RFNMQSHF8"
 BOT_PREFIX = "$"
 
-bot = commands.Bot(command_prefix = BOT_PREFIX)
+intents = discord.Intents.all()
+bot = commands.Bot(BOT_PREFIX, intents = discord.Intents.all())
 
+#CLIENT
 @bot.event
 async def on_ready():
-    print("Logged in as: " + bot.user.name + "\n")
+    print("Logged in as: " + bot.user.name)
+
+test_server = 910335283025805375
+@bot.command(name = "ping", description="PONG", guild_ids=[test_server])
+async def ping(ctx):
+    await ctx.send("PONG")
+
+#BOT
+@bot.command(name = "join")
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(name = "leave")
+async def leave(ctx):
+    voice_channel = ctx.message.guild.voice_client
+    await voice_channel.disconnect()
+
+
 
 
 bot.run(TOKEN)
